89ffc0a5239a98652f48c295b88db3a143ad5d6a
Omid Tarabavar <omidtarab1300@gmail.com>
2023-05-30 16:33:30 +0430

    Add Week1 l1 folder

---
+++ Week1/CourseIntroduction/HelloWorld.java
@@ -0,0 +1,9 @@
+package Week1.CourseIntroduction;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        StdOut.println("Hello, World");
+    }
+}
