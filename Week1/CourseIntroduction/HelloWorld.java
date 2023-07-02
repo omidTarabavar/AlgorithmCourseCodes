867867f6ccc24617ea7f1d5b6fe1a4523543ede9
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-02 13:23:33 +0430

    Changed Dir

--- Week1/CourseIntroduction/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package Week1.CourseIntroduction;
-
-import edu.princeton.cs.algs4.StdOut;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        StdOut.println("Hello, World");
-    }
-}
