d721f490a00c1640418de1a9bb228e00d20ebba5
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-12 08:59:42 +0430

    Move files for Week1, Add ThreeSum

---
+++ ThreeSum.java
@@ -0,0 +1,33 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class ThreeSum {
+    static final int MAX = 100;
+    public static void main(String[] args) {
+        for(int i = 1; i <= 16000; i *= 2){
+            System.out.println("N: "+i+"\tTime: "+timeTrial(i));
+        }
+    }
+    public static double timeTrial(int n){
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = StdRandom.uniformInt(-MAX,MAX);
+        }
+        Stopwatch stopwatch = new Stopwatch();
+        count(nums);
+        return stopwatch.elapsedTime();
+    }
+    public static int count(int[] nums) {
+        int result = 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                        result++;
+                }
+            }
+        }
+        return result;
+    }
+}
