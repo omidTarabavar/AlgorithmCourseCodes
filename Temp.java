a64506c411d683e7a3355b1c6f9d256df385df90
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-18 23:36:51 +0430

    Add our implementation of LinkedStackOfStrings

--- Temp.java
+++ Temp.java
@@ -1,18 +1,18 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
 import edu.princeton.cs.algs4.UF;
 
 public class Temp {
     public static void main(String[] args) {
-        int N  = StdIn.readInt();
-        UF uf = new UF(N);
-        while (!StdIn.isEmpty()){
-            int p = StdIn.readInt();
-            int q = StdIn.readInt();
-            if(!uf.connected(p,q)){
-                uf.union(p,q);
-                StdOut.println(p + " "+ q);
-            }
+        int a = 1;
+        int b = 2;
+        int c;
+        Stopwatch stopwatch = new Stopwatch();
+        for(int i = 0; i < 1000000; i++){
+            c = a+b;
         }
+        double time = stopwatch.elapsedTime();
+        System.out.println(time);
     }
 }
