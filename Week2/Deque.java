3987156558d868eb57cc16f35b1675bd6d2a5013
Omid Tarabavar <omidtarab1300@gmail.com>
2023-07-26 14:48:57 +0430

    Move Week2 to files

---
+++ Week2/Deque.java
@@ -0,0 +1,120 @@
+package Week2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node first = null;
+    private Node last = null;
+    private int n = 0;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException();
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.prev = null;
+        first.next = oldFirst;
+        if (isEmpty()) {
+            last = first;
+        } else
+            oldFirst.prev = first;
+        n++;
+    }
+
+    public void addLast(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException();
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        last.prev = oldLast;
+        if (isEmpty())
+            first = last;
+        else
+            oldLast.next = last;
+        n++;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty())
+            last = first;
+        else
+            first.prev = null;
+        return item;
+    }
+
+    public Item removeLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        Item item = last.item;
+        last = last.prev;
+        n--;
+        if (isEmpty())
+            first = last;
+        else
+            last.next = null;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Item next() {
+            if (isEmpty() || !hasNext())
+                throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addLast(2);
+        StdOut.println(deque.size());
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeLast());
+        StdOut.println(deque.isEmpty());
+    }
+}
